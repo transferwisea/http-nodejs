064f07a4e6201d493d517e0fe53699d996032104
transferwise <89956937+transferwisea@users.noreply.github.com>
2024-08-11 12:37:01 +0800

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 import { createServer } from 'http';
 
 createServer((req, res) => {
-  res.write('Hello World! Proof of Concept for Bug bounty Report');
+  res.write('Hello World Proof of Concept for Bug bounty Report');
   res.end();
 }).listen(process.env.PORT);
